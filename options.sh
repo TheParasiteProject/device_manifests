4c22edd7230b42115afa79d958a1e8df5771b102
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-10-06 16:31:46 +0900

    [TMP] Staging
    Change-Id: Id2b74f5e58a850bd59cec36debb6a93fc08e38ba

---
+++ options.sh
@@ -0,0 +1,10 @@
+# !/usr/bin/env bash
+#
+# Copyright (C) 2025, TheParasiteProject
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MANIFEST_URL="git@github.com:TheParasiteProject-Staging/manifest.git"
+BRANCH="main"
+JOBS="$(nproc)"
+VARIANT="user"
